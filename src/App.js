16deb113066b1dc83baf494da8adc66874e90630
jyotijauhari <jyotijauhari222@gmail.com>
2022-09-23 11:41:32 +0530

    Add. routing for different pages

--- src/App.js
+++ src/App.js
@@ -4,14 +4,25 @@ import Navbar from './Components/Navbar';
 import Banner from './Components/Banner';
 import Movies from './Components/Movies';
 import Favourite from './Components/Favourite';
+import {BrowserRouter as Router,Switch,Route, BrowserRouter} from 'react-router-dom';
+
 function App() {
   return (
-    <>
-    <Navbar/>
-    {/* <Banner/>
-    <Movies/> */}
-    <Favourite/>
-    </>
+    <Router>
+    <Navbar/>  
+    <Switch>
+    <Route path='/' exact render={(props)=>(
+      <>
+        <Banner {...props}/>
+        <Movies {...props}/>
+      </>
+    )}/>
+    <Route path='/favourites' component={Favourite}/>
+    </Switch>
+      {/* <Banner/>
+      <Movies/>
+      <Favourite/> */}
+    </Router>
   );
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 export default class Navbar extends Component {
   render() {
     return (
       <div style={{display:'flex', padding:'0.5'}}>
-        <h1>Movies App</h1>
-        <h2 style={{marginLeft:'2rem', marginTop:'1.5rem'}}>Favourite</h2>
+        <Link to="/" style={{textDecoration:'none'}}><h1>Movies App</h1></Link>
+        <Link to="/favourites" style={{textDecoration:'none'}}><h2 style={{marginLeft:'2rem', marginTop:'1.5rem' }}>Favourite</h2></Link>
       </div>
     )
   }
